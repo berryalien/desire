6469774bdb589049e130ece82f72a0e9ac9a838d
Christina <150205323+berryalien@users.noreply.github.com>
2024-07-19 02:27:52 +0400

    The adaptive  contact page is complete

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,18 @@ $(function () {
   $('.header__btn').on('click', function () {
     $('.rightside-menu').removeClass('rightside-menu--close');
   });
-  
+
   $('.rightside-menu__close').on('click', function () {
     $('.rightside-menu').addClass('rightside-menu--close');
   });
 
-    $('.header__btn-menu').on('click', function () {
-      $('.menu').toggleClass('menu--open');
-    });
+  $('.header__btn-menu').on('click', function () {
+    $('.menu').toggleClass('menu--open');
+  });
 
-if($(window).width() < 651){
-  $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
-}
+  if ($(window).width() < 651) {
+    $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+  }
 
   $('.top__slider').slick({
     dots: true,
@@ -26,12 +26,41 @@ if($(window).width() < 651){
     slidesToShow: 10,
     slidesToScroll: 10,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [{
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        }
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      }
+    ]
   });
 
   $('.article-slider__box').slick({
-prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/back.svg" alt="arrow left"></button>',
-nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/blog/next.svg" alt="arrow right"></button>'
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/back.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/blog/next.svg" alt="arrow right"></button>'
   });
 
 
